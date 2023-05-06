dacbc156dbf8fc6c99e0ad9b67658ee0a92003d6
yashpp1210 <yash8446160367@gmail.com>
2023-05-06 10:08:55 +0530

    Routing done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 //This is the starting point of any application
 import React from "react";
-import { ReactDOM } from "react-dom";
+import  ReactDOM  from "react-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 
 import App from './App';
+import 'antd/dist/antd';
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+// ReactDOM.render(<App/>,document.getElementById('root'));
+
+ReactDOM.render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById('root')
+  )
